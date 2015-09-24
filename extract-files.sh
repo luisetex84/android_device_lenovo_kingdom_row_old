1a655eb9768b7a708e22ffca0723f312e044cd45
Luis de Lucas <luisetex84@outlook.com>
2015-09-24 09:41:06 +0200

    Added vendor config

---
+++ extract-files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+        DIR=`dirname $FILE`
+        if [ ! -d $2/$DIR ]; then
+            mkdir -p $2/$DIR
+        fi
+        # Try CM target first
+        adb pull /system/$DEST $2/$DEST
+        # if file does not exist try OEM target
+        if [ "$?" != "0" ]; then
+            adb pull /system/$FILE $2/$DEST
+        fi
+    done
+}
+
+#Use Traditional sorting
+export LC_ALL=C
+
+FP=$(cd ${0%/*} && pwd -P)
+export VENDOR=$(basename $(dirname $FP))
+export DEVICE=$(basename $FP)
+
+BASE=../../../vendor/lenovo/msm8974-common/proprietary
+rm -rf $BASE/*
+
+DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $DEVBASE/*
+
+extract ../../lenovo/kingdom_row/proprietary-files-qc.txt $BASE
+extract ../../lenovo/kingdom_row/proprietary-files.txt $DEVBASE
+extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt $DEVBASE
+
